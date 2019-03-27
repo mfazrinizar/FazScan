a7e88ac1edf02767b711327750367bbc864cbaac
M.Fazri Nizar <anon6372098.id@gmail.com>
2019-03-27 13:33:53 +0700

    Install the Required Perl Module(s)

---
+++ installpm.sh
@@ -0,0 +1,12 @@
+cpan install LWP::Bundle
+cpan install LWP::UserAgent
+cpan install LWP::Simple
+cpan install HTTP::Response
+cpan install Getopt::Long
+cpan install HTTP::Request
+cpan install IO::Select
+cpan install HTTP::Cookies
+cpan install HTTP::Response
+cpan install HTTP::Request::Common
+cpan install URI::URL
+cpan install IO::Socket::INET
